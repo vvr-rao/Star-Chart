22767f5d35803ecb57edc11977c5d089c224293e
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2021-11-04 11:35:11 -0400

    Add files via upload

---
+++ Lambda-to-search-Athena-based-on-Catalog-Id.py
@@ -0,0 +1,62 @@
+import json
+import boto3
+import time
+
+# Can expose this VIA API Gateway as a GET call
+# {
+#     "Catalog": "$input.params('Catalog')",
+#     "CatalogId": "$input.params('CatalogId')"
+# }
+
+def lambda_handler(event, context):
+    #return("Got event\n" + json.dumps(event, indent=2))
+    cat = event['Catalog']
+    cat_id = event['CatalogId']
+    #cat = "NGC"
+    #cat_id = "2261"
+    my_query = "SELECT a.AngularDist, d.hd, d.type, d.ApMagnitude, d.Constellation, d.name, \
+        d.cat1, d.id1  FROM \"AwsDataCatalog\".\"starchart\".\"distances\" a, \"AwsDataCatalog\".\"db_name\".\"table_name\" s, \
+        \"AwsDataCatalog\".\"db_name\".\"table_name\" d   \
+        where s.cat1 = \'" + cat + "\' and s.id1 = " + cat_id + " and a.Source = s.RowId   \
+        and a.Destination = d.RowId  \
+        ORDER BY (a.AngularDist) \
+        LIMIT 20"
+        
+    database_name = "<ATHENA_DATABASE>"
+    catalog_name = "<ATHENA_CATALOG>"
+    
+    athena_client = boto3.client('athena')
+    
+    QueryResponse = athena_client.start_query_execution(
+        QueryString = my_query,
+        QueryExecutionContext={
+            "Database": database_name,
+            "Catalog": catalog_name
+        },
+        ResultConfiguration={
+            'OutputLocation': 's3://<BUCKET>/<FOLDER>/'
+            }
+        
+        
+        )
+        
+    QueryId = QueryResponse["QueryExecutionId"]
+    
+    time.sleep(5)
+    
+    
+    query_results = athena_client.get_query_results(
+            QueryExecutionId=QueryId,
+            MaxResults=12
+        
+        )
+    
+    out = "result: "
+        
+    #for row in query_results['ResultSet']['Row']:
+     #   print(row)
+     #   out = out + row
+    #json_object =  json.loads(query_results) 
+    
+        
+    return (query_results["ResultSet"])
